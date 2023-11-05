ffc14bde6ae90a2eb76c8a14dbf9e84dad29e8fa
ishanhimalka <ishanhimalka1@gmail.com>
2023-11-05 22:01:45 +0530

    update

--- crud-with-mongodb/app.js
+++ crud-with-mongodb/app.js
@@ -4,6 +4,7 @@ const connectDB = require('./db');
 const careerRouter = require("./routes/CareersRoutes");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const router = require("./routes/router");
 const newsRoutes = require("./routes/NewsRoutes");
 
 const app = express();
@@ -23,9 +24,11 @@ app.use("/api/Carrers", careerRouter);
 app.use("/api/News", newsRoutes);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+
 });
 
 module.exports = app;
