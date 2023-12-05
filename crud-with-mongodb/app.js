73bd64f82ac59220af30c2d19c2411419207d3a0
ishanhimalka <ishanhimalka1@gmail.com>
2023-12-05 18:18:11 +0530

    all

--- crud-with-mongodb/app.js
+++ crud-with-mongodb/app.js
@@ -6,7 +6,13 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const router = require("./routes/router");
 const newsRoutes = require("./routes/NewsRoutes");
+const https = require("https"),
+  fs = require("fs");
 
+// const options = {
+//   key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
+//   cert: fs.readFileSync("/srv/www/keys/chain.pem")
+// };
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -20,18 +26,20 @@ require('dotenv').config();
 connectDB();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://salemalmakrani.net'
-}));
-app.use(cors({
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
+
+const corsOptions = {
+  origin: 'https://salemalmakrani.net',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+
 app.use("/api/Carrers", careerRouter);
 app.use("/api/News", newsRoutes);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use(router);
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 
